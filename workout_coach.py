861c0aab32133cad9b93172b81365666a802a46e
batterto <battmatt@gmail.com>
2013-05-13 22:14:51 -0700

    create Coach class

--- workout_coach.py
+++ workout_coach.py
@@ -2,25 +2,45 @@
 
 import subprocess, time
 
-exercises = ["jumping jacks",
-              "wall sit",
-              "push up",
-              "crunch",
-              "step up on chair",
-              "squat",
-              "triceps dip",
-              "plank",
-              "high knees",
-              "lunge",
-              "push up and rotate",
-              "side plank"]
 
-for exercise in exercises:
-    time.sleep(1)
-    subprocess.call(["say", exercise])
-    time.sleep(30)
-    if (exercise != "side plank"):
-        subprocess.call(["say", "rest"])
-        time.sleep(9)
+class Coach(object):
+    def __init__(self, exercises=[], workout_time=30, rest_time=10):
+        self.exercises = exercises
+        self.workout_time = workout_time
+        self.rest_time = rest_time
 
-subprocess.call(["say", "Done"])
+    def run_workout(self):
+
+        for exercise in self.exercises:
+            time.sleep(1)
+            subprocess.call(["say", exercise])
+            time.sleep(self.workout_time)
+            #TODO think of better way to skip the last exercise
+            if (exercise != "side plank"):
+                subprocess.call(["say", "rest"])
+                time.sleep(self.rest_time - 1)
+
+        subprocess.call(["say", "Done"])
+    
+
+
+if __name__ == '__main__':
+    coach = Coach(exercises=["jumping jacks",
+                             "wall sit",
+                             "push up",
+                             "crunch",
+                             "step up on chair",
+                             "squat",
+                             "triceps dip",
+                             "plank",
+                             "high knees",
+                             "lunge",
+                             "push up and rotate",
+                             "side plank"])
+    coach.run_workout()
+
+
+
+
+
+
